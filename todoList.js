6d2e096f9f88798062e024b5493a402d9288deed
csdc23vz_dworscaka <andreas.dworscak@stud.fh-campuswien.ac.at>
2023-01-15 17:15:13 +0100

    Finished

--- todoList.js
+++ todoList.js
@@ -1,30 +1,34 @@
 let id_increment = 0
 
-let html_task_container = document.getElementById("tasks-container")
+let html_task_container = document.getElementById("tasks_container-container")
 let html_task_input = document.getElementById("task_input")
 let html_debug_out = document.getElementById("debug_out")
 
 function add_task(){
     // if input value length greater 0, execute. If 0, string is empty, interpreted as false
     if(html_task_input.value){
-        //create html task element and its content
+        //element definition
         let new_task = document.createElement("div")
-        new_task.id = id_increment.toString()
-
         let new_task_checkbox = document.createElement("input")
         new_task_checkbox.type = "checkbox"
-        new_task_checkbox.style.display = "inline-block"
-
         let new_task_content = document.createElement("p")
-        new_task_content.innerText = html_task_input.value
-        new_task_content.style.display = "inline-block"
 
+        //give new_task an ID
+        new_task.id = id_increment.toString()
+
+        //Write what is written in the input box, into the new task
+        new_task_content.innerText = html_task_input.value
+
+        //add the new elements into the DOM tree
         new_task.appendChild(new_task_checkbox)
         new_task.appendChild(new_task_content)
-
         html_task_container.appendChild(new_task)
 
         id_increment++
+
+        //Styling
+        new_task_checkbox.style.display = "inline"
+        new_task_content.style.display = "inline"
 
         function cross_out_task(){
             if(new_task_content.style.textDecoration !== "line-through"){
